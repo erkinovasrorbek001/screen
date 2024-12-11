96ee4a7377fd353b38e34946691a828780760241
erkinovasrorbek001 <erkinovasrorbek3@gmail.com>
2024-12-11 16:14:22 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,25 @@
+ //1 masala
+ let R = 8;
+ let π = 3;
+
+ S = π * R^2
+ L= 2 * π * R
+
+ console.log(S,  L);
+
+//2 masala
+let a =2;
+let b =6;
+
+G = a * b
+
+console.log(a,b);
+
+//3 masala
+let x=3;
+let y=8;
+
+y = 3 * x^6 - 6 * x^2 - 7
+ 
+console.log(x, y);
+
